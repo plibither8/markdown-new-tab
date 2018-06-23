be8baa48540309b38b29ff1352b2d087d493c029
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-23 10:47:18 +0530

    Add options to renderer and set flavour to github

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,4 +1,18 @@
-const converter = new showdown.Converter();
+const converter = new showdown.Converter({
+    'simplifiedAutoLink': true,
+    'excludeTrailingPunctuationFromURLs': true,
+    'strikethrough': true,
+    'tables': true,
+    'tasklist': true,
+    'ghCodeBlocks': true,
+    'smoothLivePreview': true,
+    'smartIndentationFix': true,
+    'simpleLineBreaks': true,
+    'openLinksInNewWindow': true,
+    'emoji': true
+});
+
+converter.setFlavor('github');
 
 const renderBox = document.querySelector('.text');
 const textarea = document.querySelector('textarea');
