b2d5c8fa13e8ef28066ffbe7102465e2bf21eeb9
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-25 10:54:15 +0530

    package: Add babel and minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
+const babel         = require("gulp-babel");
 const concat        = require("gulp-concat");
 const gulp          = require("gulp");
+const minify        = require('gulp-minify');
 const pug           = require("gulp-pug");
 const stylus        = require("gulp-stylus");
 
@@ -19,8 +21,11 @@ gulp.task('styl', () => {
 
 gulp.task('js', () => {
     return gulp.src('src/js/*.js')
-        .pipe(concat('index.js'))
+        .pipe(babel({
+            presets: ['env']
+        }))
         .pipe(gulp.dest('dist/assets/js'))
+
 })
 
 gulp.task('default', ['pug', 'styl', 'js'], () => {
