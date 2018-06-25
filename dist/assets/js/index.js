1910b1112913f3837eb03a70be76519d3eaa6e69
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-25 10:54:33 +0530

    Add bottom bar and minimalise to black and white monotone

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,4 +1,13 @@
-const converter = new showdown.Converter({
+'use strict';
+
+/**
+ * Initiate the markdown renderer
+ * with specified options
+ *
+ * TODO: Allow user to manually config
+ * these options
+ */
+var converter = new showdown.Converter({
     'simplifiedAutoLink': true,
     'excludeTrailingPunctuationFromURLs': true,
     'strikethrough': true,
@@ -12,51 +21,105 @@ const converter = new showdown.Converter({
     'emoji': true
 });
 
+/**
+ * GitHub-styled markdown to allow
+ * tasklists, tables, simple-line-breaks etc.
+ */
 converter.setFlavor('github');
 
-const edit = () => {
-    textarea.classList.remove('nodisplay');
-    renderBox.classList.add('nodisplay');
-    textarea.focus();
+/**
+ *
+ */
+var renderBox = document.querySelector('.markdown-body');
+var textarea = document.querySelector('textarea');
+
+var toggleDisplay = function toggleDisplay(n) {
+
+    if (n) {
+        textarea.classList.remove('nodisplay');
+        renderBox.classList.add('nodisplay');
+    } else {
+        textarea.classList.add('nodisplay');
+        renderBox.classList.remove('nodisplay');
+    }
 };
 
-const save = () => {
-    textarea.classList.add('nodisplay');
-    renderBox.classList.remove('nodisplay');
-    text = textarea.value;
-    html = converter.makeHtml(text);
+var moveCaretToStart = function moveCaretToStart() {
+
+    if (typeof textarea.selectionStart === "number") {
+        textarea.selectionStart = textarea.selectionEnd = 0;
+    } else if (typeof textarea.createTextRange !== "undefined") {
+        textarea.focus();
+        var range = textarea.createTextRange();
+        range.collapse(true);
+        range.select();
+    }
+};
+
+var edit = function edit() {
+
+    toggleDisplay(1);
+    moveCaretToStart();
+    textarea.focus();
+    textarea.scrollTop = 0;
+};
+
+var save = function save() {
+
+    toggleDisplay(0);
+
+    var text = textarea.value;
+    var html = converter.makeHtml(text);
+
     renderBox.innerHTML = html;
     localStorage.setItem("rawText", text);
-}
+};
 
-const renderBox = document.querySelector('.markdown-body');
-const textarea = document.querySelector('textarea');
-const rawText = localStorage.getItem("rawText");
+var rawText = localStorage.getItem("rawText");
 textarea.value = rawText;
 save();
 
-textarea.addEventListener('input', () => {
+textarea.addEventListener('input', function () {
     text = textarea.value;
     html = converter.makeHtml(text);
     renderBox.innerHTML = html;
-})
+});
 
-const editButton = document.querySelector('#edit');
-const saveButton = document.querySelector('#save');
+var editButton = document.querySelector('#edit');
+var saveButton = document.querySelector('#save');
 
 editButton.addEventListener('click', edit);
 saveButton.addEventListener('click', save);
 
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        save();
+document.addEventListener("keydown", function (e) {
+    if (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) {
+        if (e.keyCode === 83) {
+            e.preventDefault();
+            save();
+        } else if (e.keyCode === 69) {
+            e.preventDefault();
+            edit();
+        }
     }
 }, false);
 
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 69 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        edit();
-    }
-}, false);
+var timeDisplay = function timeDisplay() {
+
+    setInterval(function () {
+
+        var today = new Date();
+        var day = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+        var month = today.getMonth() + 1 < 10 ? "0" + (today.getMonth() + 1) : today.getMonth() + 1;
+        var year = today.getFullYear() < 10 ? "0" + today.getFullYear() : today.getFullYear();
+
+        var hour = today.getHours() < 10 ? "0" + today.getHours() : today.getHours();
+        var minute = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+        var seconds = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+
+        var output = day + '/' + month + '/' + year + ' - ' + hour + ':' + minute + ':' + seconds;
+
+        document.querySelector('#time').innerHTML = output;
+    }, 1000);
+};
+
+timeDisplay();
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
+/**
+ * Initiate the markdown renderer
+ * with specified options
+ *
+ * TODO: Allow user to manually config
+ * these options
+ */
 const converter = new showdown.Converter({
     'simplifiedAutoLink': true,
     'excludeTrailingPunctuationFromURLs': true,
@@ -12,25 +19,70 @@ const converter = new showdown.Converter({
     'emoji': true
 });
 
+/**
+ * GitHub-styled markdown to allow
+ * tasklists, tables, simple-line-breaks etc.
+ */
 converter.setFlavor('github');
 
+
+
+/**
+ *
+ */
+const renderBox = document.querySelector('.markdown-body');
+const textarea = document.querySelector('textarea');
+
+const toggleDisplay = (n) => {
+
+    if (n) {
+        textarea.classList.remove('nodisplay');
+        renderBox.classList.add('nodisplay');
+    }
+
+    else {
+        textarea.classList.add('nodisplay');
+        renderBox.classList.remove('nodisplay');
+    }
+
+}
+
+const moveCaretToStart = () => {
+
+    if (typeof textarea.selectionStart === "number") {
+        textarea.selectionStart = textarea.selectionEnd = 0;
+    }
+
+    else if (typeof textarea.createTextRange !== "undefined") {
+        textarea.focus();
+        const range = textarea.createTextRange();
+        range.collapse(true);
+        range.select();
+    }
+
+}
+
 const edit = () => {
-    textarea.classList.remove('nodisplay');
-    renderBox.classList.add('nodisplay');
+
+    toggleDisplay(1);
+    moveCaretToStart();
     textarea.focus();
+    textarea.scrollTop = 0
+
 };
 
 const save = () => {
-    textarea.classList.add('nodisplay');
-    renderBox.classList.remove('nodisplay');
-    text = textarea.value;
-    html = converter.makeHtml(text);
+
+    toggleDisplay(0);
+
+    const text = textarea.value;
+    const html = converter.makeHtml(text);
+
     renderBox.innerHTML = html;
     localStorage.setItem("rawText", text);
+
 }
 
-const renderBox = document.querySelector('.markdown-body');
-const textarea = document.querySelector('textarea');
 const rawText = localStorage.getItem("rawText");
 textarea.value = rawText;
 save();
@@ -48,15 +100,40 @@ editButton.addEventListener('click', edit);
 saveButton.addEventListener('click', save);
 
 document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        save();
+    if (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) {
+        if (e.keyCode === 83) {
+            e.preventDefault();
+            save();
+        }
+        else if (e.keyCode === 69) {
+            e.preventDefault();
+            edit();
+        }
     }
 }, false);
 
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode === 69 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        edit();
-    }
-}, false);
+
+
+const timeDisplay = () => {
+
+    setInterval(function () {
+
+        const today = new Date();
+        const day = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+        const month = (today.getMonth() + 1) < 10 ? "0" + (today.getMonth() + 1) : (today.getMonth() + 1);
+        const year = today.getFullYear() < 10 ? "0" + today.getFullYear() : today.getFullYear();
+
+        const hour = today.getHours() < 10 ? "0" + today.getHours() : today.getHours();
+        const minute = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+        const seconds = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+
+        const output = day + '/' + month + '/' + year + ' - ' +
+            hour + ':' + minute + ':' + seconds;
+
+        document.querySelector('#time').innerHTML = output;
+
+    }, 1000);
+
+};
+
+timeDisplay();
